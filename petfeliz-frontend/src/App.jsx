17b3a75b061c075b4ba2a1bf5a364923a4147394
MarianaPizarro14 <maripizarro1418@gmail.com>
2026-08-27 23:35:56 -0500

    feat: integra Vercel Analytics en el frontend

--- petfeliz-frontend/src/App.jsx
+++ petfeliz-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import ScrollToTop from './components/ScrollToTop'
@@ -184,6 +185,7 @@ function App() {
     <BrowserRouter>
       <UserProvider>
         <AppContent />
+        <Analytics />
       </UserProvider>
     </BrowserRouter>
   )

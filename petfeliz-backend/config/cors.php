c06651b0d4d74887541d924aa9994fae7cc87b87
MarianaPizarro14 <maripizarro1418@gmail.com>
2026-09-05 18:41:37 -0500

    fix: agregar dominio epspetfeliz.site a CORS allowed_origins

--- petfeliz-backend/config/cors.php
+++ petfeliz-backend/config/cors.php
@@ -9,10 +9,13 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        env('FRONTEND_URL', 'https://eps-pet-feliz.vercel.app'),
+        'https://www.epspetfeliz.site',
+        'https://epspetfeliz.site',
+        env('FRONTEND_URL', 'https://www.epspetfeliz.site'),
     ],
 
     'allowed_origins_patterns' => [
+        '#^https://(www\.)?epspetfeliz\.site$#',
         '#^https://.*\.vercel\.app$#',
     ],
 
